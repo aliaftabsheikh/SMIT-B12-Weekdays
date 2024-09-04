860f5f131f9d7fbc79695cdd6eb369d63bdb299b
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-09-04 18:59:13 +0500

    js-class-1

---
+++ module 2/class-1/app.js
@@ -0,0 +1,11 @@
+// alert("Hello World 123")
+
+// var fname;     //Variable Declare
+// fname = "Ali Aftab";     // Value Initialize
+
+
+// var fname = "Ali Aftab Sheikh";   
+// console.log(fname)
+
+
+// alert(fname)
