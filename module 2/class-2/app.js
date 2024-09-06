d5f73e537233b5ea4967e12709c63d2d9a953797
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-09-06 18:57:58 +0500

    js-class-2

---
+++ module 2/class-2/app.js
@@ -0,0 +1,82 @@
+// alert("Hello World")
+// console.log("Hello World")
+
+
+
+// var fname;
+
+// fname = "ALI Aftab"
+
+// alert(fname)
+
+
+
+// num = num + 25
+
+// console.log(num);
+
+
+// console.log(num);
+
+// var num = 25;
+// var num2 = 23;
+// var num3 = num + num2
+
+// console.log(num3);
+// console.log(num2);
+// console.log(num);
+
+// var firstNumber;  //Camel Case
+// var FirstNumber;  // Pascal Case
+// var first_number;  // Snake Case
+
+// console.log(num);
+
+
+ //Operands num
+//Operators + - * / %
+
+
+//! Addition
+
+// var num = 5;
+// var num2 = 10;
+// var num3 = 5
+
+// console.log(num + num2 + num3);
+
+//! Subtraction 
+
+// var num = 25;
+// var num2 = 10;
+
+// console.log(num - num2);
+
+//! Multiplication
+           
+// var num = 5;
+// var num2 = 5;
+
+// console.log(num * num2);
+
+//! Multiplication
+           
+// var num = 5;
+// var num2 = 5;
+
+// console.log(num * num2);
+
+//! Division
+
+// var num = 30;
+// var num2 = 5;
+
+// console.log(num2 / num);
+
+
+//! Modulus
+
+var num = 9;
+var num2 = 3;
+
+console.log(num2 % num);
