7f70a0fcb6f74803e0973b35e4c2d0e8044a8117
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-09-09 18:56:18 +0500

    js-class-3

---
+++ module 2/class-3/app.js
@@ -0,0 +1,45 @@
+
+//!Post Increment
+
+// var num = 1;
+// console.log(num++);
+
+// num++;
+// console.log(num);
+
+//!Pre Increment
+
+// var num = 1;
+// ++num;  // 2
+// console.log(num++); //Print > Increment
+// console.log(++num); // Increment > Print 
+
+
+//! Post Decrement
+
+// var num = 5;
+// num--;
+// console.log(num--);
+// console.log(num);
+
+//! Pre Decrement
+
+// var num = 5;
+// --num;
+// console.log(num);
+
+
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+
+// var num = 4 - 2 + 5 * 2;
+        // 4 - 2 + 10
+        // 4 + 8
+        // 12
+  
+// var num = ((2 * 4) * 4) + 2
+// var num = 5 + 3 * 2 - 4 / 2
+var num = (-5 + 3) * (2 - 4) / 2
+
+console.log(num);
