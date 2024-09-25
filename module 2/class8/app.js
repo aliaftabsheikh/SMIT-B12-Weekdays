ce32d87d71686189c7a66b6ec1789583a9c2bfa6
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-09-25 18:57:18 +0500

    js-class-8

---
+++ module 2/class8/app.js
@@ -0,0 +1,67 @@
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+// console.log("SMIT B12");
+
+
+// FOR LOOP
+
+// for(var i = 1; i <= 5; i++){
+//     console.log("SMIT Batch12");
+
+// }
+
+
+
+// var fruits = ["Apple", "Mango", "Banana", "Grapes", "BB", "Kiwi", "Papaya"]
+
+// for(var i = 0; i < fruits.length; i++){
+//     console.log(i + 1 , fruits[i]);
+// }
+
+// var table = 2;
+
+// for(var i = 10; i >= 1; i--){Mango
+//     console.log(table + " X " + i + " = " + table * i );
+// }
+
+// for (var i = 1; i <= 5; i++) {
+//     if (i == 2) {
+//         continue;
+//     // console.log("Please be prepared for chapter", i);
+//     }
+    
+// console.log(i);
+// }
+
+// var fruits = ["Apple", "", "Banana", "Grapes", "BB", "Kiwi", "Papaya"]
+
+// for(var i = 0; i < fruits.length; i++){
+//   if("Banana" == fruits[i]){
+//     console.log("Banana Found at index", i);
+//     break;
+//   }else{
+//     console.log(i);
+//     }
+// }
+
+// Outer loop for numbers 1 through 5
+for (let i = 1; i <= 2; i++) {
+    console.log(`Multiplication table for ${i}:`);
+
+    // Inner loop for multiplying each number by 1 to 10
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} X ${j} = ${i * j}`);
+    }
+
+    // Add an empty line for better formatting between tables
+    console.log('');
+}
+
+
