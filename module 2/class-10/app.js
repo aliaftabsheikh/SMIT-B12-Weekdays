d94442161ac441c207898a0d769fc28d02c0b76a
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-10-02 18:43:55 +0500

    js-class-10

---
+++ module 2/class-10/app.js
@@ -0,0 +1,14 @@
+// var cityName = prompt("Enter your city name", "Karachi").toUpperCase()
+
+// // cityName = cityName.toLowerCase();
+
+// console.log(cityName);
+
+// var cities = ["islamabad", "lahore", "karachi", "peshawar", "quetta"];
+
+// for(var i = 0; i < cities.length; i++){
+//     if(cityName == cities[i]){
+//         alert("City Found");
+//     }
+
+// }
---
+++ module 2/class-9/app.js
@@ -0,0 +1,77 @@
+// for(var i = 0; i < 10; i++){
+//     console.log(i + 1, "Please follow me on github !");
+// }
+
+// var fruits = ["Apple", "Mango", "Banana", "Grapes", "BB", "Kiwi", "Papaya"]
+
+// for(var i = 0; i < fruits.length; i++){
+//       if("Banana" == fruits[i]){
+//         console.log("Banana Found at index", i);
+//         break;
+//       }else{
+//         console.log(i);
+//         }
+//     }
+
+// console.log(fruits[0]);
+
+
+// for(var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// var table = +prompt("Enter a Number", 2)
+
+// for(var i = 1; i <=10; i++){
+//     console.log(table + " X " + i + " = " + table * i);
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+
+
+    
+
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+// var fullNames = [];
+
+// for (var i = 0; i < firstNames.length; i++) {
+
+//     for (var j = 0; j < lastNames.length; j++) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+
+
+
+// }
+
+// console.log(fullNames);
+
+
+for (var i = 1; i <= 10; i++) {
+    var row = "";
+    for (var j = 1; j <= i; j++) {
+        row += "*"
+    }
+    console.log(row);
+}
+
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(`Multiplication table for ${i}:`);
+
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} X ${j} = ${i * j}`);
+//     }
+
+//     console.log('');
+// }
+
+
+// for (var i = 1; i <= 5; i++) {
+//         if (i == 3) {
+//             return;
+//         }
+        
+//     console.log(i);
+//     }
