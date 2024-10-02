941bb4c3de5171523e8dfcfca3ad759523a40897
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-10-02 18:42:44 +0500

    js-class-10

---
+++ module 2/class-11/app.js
@@ -0,0 +1,43 @@
+var text = "MY name is Ali Aftab"
+
+// console.log(text.length);
+// var findName = text.lastIndexOf("A")
+// console.log(findName)
+
+// console.log(text.slice(11, 14));
+// console.log(text.slice(findName, findName + 3));
+
+
+// console.log(text.slice(11, 14 ));
+// var text = "In 1939 World War II happens "
+
+//  for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 12) === "World War II") {
+//      text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//      }
+//  }
+// var modifyText = text.replace("Ali Aftab", "Jethanand Bhai !")
+
+// var name = "Ubaid";
+
+// console.log(name.slice(4, -1));
+// console.log(name.charAt(name.length - 1 ));  //Last Character
+
+// var text = "Hello ! My name is Ali !!"
+
+// for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//         console.log("Exclamation point found!");
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// var text = "This is Cat. Cat is very cute !"
+
+// console.log(text.replace(/Cat/g, "Parrot"));
+
+
+var number = 80.4
+
+console.log(Math.round(number));
