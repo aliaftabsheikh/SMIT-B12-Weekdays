e7ab0776f2a75983fe4e2eb74af47ce5c96121fd
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-10-09 18:58:51 +0500

    class-14

---
+++ module 2/class-14/app.js
@@ -0,0 +1,82 @@
+// Function Definition
+
+function printMyName() {
+    console.log("Ali Aftab");
+}
+
+//Function Calling 
+// printMyName() 
+// for(var i = 0; i < 5; i++){
+//     console.log(i); 
+// }
+// printMyName()
+
+// function sum (){
+//     console.log(2 + 2)
+// }
+
+// function tellTime(){
+//     var now = new Date();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//     var theSec = now.getSeconds();
+//     var theMSec = now.getMilliseconds();
+//     console.log("Current time: " + theHr + ":" + theMin + ":" + theSec + ":" + theMSec);
+// }
+
+// tellTime()
+
+
+function printFName(){
+    console.log("Huzaifa Abid");
+    
+}
+
+// printFName()
+// printFName()
+// printFName()
+
+
+// function printFullName(firstName, lastName){
+//     console.log(firstName + lastName);
+    
+// }
+
+// printFullName("Huzaifa", " Abid")
+// printFullName("Ahmed", " Raza")
+// printFullName("Jethnand", " Malhi")
+
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// function subtract(num1, num2){
+//     console.log(num1 - num2);
+// }
+
+// add(2, 2)
+// subtract(5, 2)
+
+const results = 10;
+
+function sum(num1, num2){
+  const results = num1 + num2
+}
+
+sum(2,2)
+    console.log(results)
+
+// function subtract(num1){
+//    return num1 - sum(5, 5)
+// }
+
+// console.log(subtract(15))
+
+
+
+// var res = sum(2,2)
+// var res1 = sum(8,12)
+
+// console.log(res);
+// console.log(res1);
