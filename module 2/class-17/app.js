90cf7c160cbedf23aaf2501af00e648a3aa46c96
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-10-19 11:25:44 +0500

    js-class-17

---
+++ module 2/class-17/app.js
@@ -0,0 +1,34 @@
+// function checkAddress(email, password) {
+
+//     if ((document.getElementById(email).value !== "") && (document.getElementById(password).value !== "")) {
+//         alert("Form Submit")
+//     } else {
+//         alert("Invalid email or password");
+
+//     }
+// }
+// function checkAddress(email, password) {
+
+//     if ((document.getElementById(email).value === "") || (document.getElementById(password).value === "")) {
+//         alert("Invalid email or password");
+//     } else {
+//         alert("Form Submit")
+
+//     }
+// }
+
+// function expandText() {
+//     var largePara = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere repellendus omnis dignissimos minus voluptatum quod quam doloribus nam. Atque commodi voluptates inventore officiis alias optio non voluptas ea culpa itaque!
+//  Impedit mollitia possimus corporis beatae minima quos. Quo mollitia, ipsam fugiat, sunt quasi qui quod voluptas dolore temporibus, ea quam facilis magnam perferendis cum assumenda ab obcaecati adipisci dolores in.
+//  Debitis autem inventore impedit perferendis, veniam laudantium eos quidem expedita repellendus consectetur illo! Repellat distinctio similique culpa. Tenetur sunt incidunt harum doloribus? Quis officia incidunt nobis cupiditate eos esse? Voluptatem.
+//  Nostrum sint facere ab porro ea. Debitis, consequatur ipsum illum vitae modi laudantium rerum dolorum non tempore minima officiis ab amet? Minima nam repudiandae quas saepe commodi veritatis. Amet, aliquid!`
+
+//     document.getElementById('para').innerHTML = largePara
+// }
+
+function makeInvisible(){
+    document.getElementById('hide').className = "hidden"
+}
+function makeVisible(){
+    document.getElementById('hide').className = "show"
+}
