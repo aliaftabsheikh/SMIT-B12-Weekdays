b46851de6a048e25efb3b4c1aa6b07d9b06fa277
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-10-25 18:58:35 +0500

    js-class-19

---
+++ module 2/class18/app.js
@@ -0,0 +1,15 @@
+//! Targrt by id
+// var button = document.getElementById('btn')
+
+// Target by className
+
+// var input = document.getElementsByClassName('inputClass')
+
+
+// Tag Name
+
+// var inputEl = document.getElementsByTagName('input')
+
+// var button = document.querySelectorAll('.inputClass')
+// var button = document.querySelector('.inputClass')
+
---
+++ module 2/class19/app.js
@@ -0,0 +1,48 @@
+// var para = document.getElementById('para-2')
+
+// var parentElem = para.parentNode;
+
+// parentElem.removeChild(para)
+
+
+// var todos = document.querySelector('#todo')
+
+// var paraElem = document.createElement('p');
+// paraElem.innerHTML = 'Todo-4';  
+// {/* <p>Todo-4</p> */}
+
+// todos.appendChild(paraElem)
+
+var idCount = 1
+
+function addTodo() {
+    var todos = document.getElementById('todos')
+    var inpVal = document.getElementById('inp')
+    var element = document.createElement('div')
+    element.setAttribute('id', idCount)
+    element.setAttribute('class', "todo")
+
+    element.innerHTML = `
+    <p>${inpVal.value}</p> <button onclick="deleteTodo(${idCount})">Delete</button> <button onclick="updateTodo(${idCount})">Update</button>
+   `
+
+    todos.appendChild(element)
+
+    idCount = idCount + 1;
+}
+
+function deleteTodo(id){
+    // console.log("DELETED", id);
+    var deletedItem = document.getElementById(id);
+    var parentElem = deletedItem.parentNode;
+    parentElem.removeChild(deletedItem)
+}
+
+function updateTodo(id){
+    var inpVal = document.getElementById('inp')
+    var todoItem = document.getElementById(id)
+
+    todoItem.innerHTML = `
+    <p>${inpVal.value}</p> <button onclick="deleteTodo(${id})">Delete</button> <button onclick="updateTodo(${id})">Update</button>
+   `
+}
