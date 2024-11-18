3c08163a9687556dbf0a9e333ae6c4e3e0832d63
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-11-18 23:37:42 +0500

    js-class-27

--- module 2/class-26/app.js
+++ module 2/class-26/app.js
@@ -37,7 +37,7 @@ function addTodo() {
 
 }
 
-function updateTodo(id){
+function updateTodo(id) {
     var element = document.getElementById(id)
     var paraElem = element.querySelector('p')
     var input = document.getElementById('inp');
@@ -52,3 +52,55 @@ function deleteTodo(id) {
     var parentElem = element.parentNode;
     parentElem.removeChild(element)
 }
+
+
+// var object = {
+//     first: 1,
+//     second: 2,
+//     first: 2,
+// }
+
+// console.log(object.first);
+
+// var num = 10;
+
+// console.log(num.toFixed(2));
+
+// var name = 'alia'
+
+// console.log(name.lastIndexOf('a'));
+
+
+// var name = "john player";
+// console.log(name[3]);
+
+// var myarray = ["foo","bar","baz"];
+// myarray.length=0;
+// myarray.push("bin");
+// console.log(myarray);
+
+// var a=7;
+// function hello() {
+//  if (true) {
+// var a=5;
+// }
+// console.log(a);
+// }
+
+// hello()
+
+// var size = 5;
+// var a = 5;
+// var size = 4;
+// for(var j=size; j>=0 ; j--){
+//   console.log(a);
+//   a = a-2;
+// }
+
+// function range(length) {
+//     var a=5;
+//   for (var i=0; i<length; i++) {
+//     console.log(a);
+//     }
+//   }
+//   range(3)
---
+++ module 2/class-27/app.js
@@ -0,0 +1,47 @@
+
+// var num = 22;
+
+// var num = 'abc'
+// var num = 'def'
+
+// function abc(){
+//     var num = 24;
+//     console.log(num);
+// }
+
+// abc()
+// console.log(num);
+
+
+// console.log(num);
+
+// {
+//     var num = 22;
+//     console.log(num);
+// }
+
+// console.log(num);
+
+
+
+function abc(){
+    let num;
+
+//    console.log(num);
+   
+}
+
+// num = 24;
+// console.log(num);
+
+// let num = 23;
+// console.log(num);
+
+
+// abc()
+
+const num = 123;
+
+// num = 24
+
+console.log(num);
