4388ae7a4ceb63ae268fd53c4e157ea754649fad
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2024-11-27 18:53:05 +0500

    class-31

--- module 2/class-30/app.js
+++ module 2/class-30/app.js
@@ -19,19 +19,44 @@
 
 
 
-function abc1(){
-    console.log("Console-1");
+
+
+// function abc1(){
+//     console.log("Console-1");
     
-}
+// }
 
-const abc2 = function(){
-    console.log("Console 2")
-}
+// var abc2 = function(){
+//     console.log("Console 2")
+// }
 
-abc1()
-abc2()
+
+
+// abc1()
+// abc2()
+
+
+//! BEFORE
+
+// let sum = (num1, num2)=>{
+    // return num1 + num2
+    // }
+
+//! After
+
+// let sum = (num1, num2)=> num1 + num2
+
+
+
+// var result = sum(5, 10)
+// console.log(result);
+
+
 
 // var abc = 21;
 
 // abc()
+
+
+
 
---
+++ module 2/class-31/app.js
@@ -0,0 +1,26 @@
+// function b(){
+//     console.log(10);
+
+// }
+
+// var a = 10
+// console.log(window.a)
+// console.log(a);
+
+
+// console.log(this);
+
+var x = 10;
+
+function a() {
+        c()
+
+    function c() {
+        console.log(b);
+        
+    }
+
+}
+
+var b = 10;
+a()
