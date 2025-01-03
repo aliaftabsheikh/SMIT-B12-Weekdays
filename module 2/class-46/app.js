0ddbaa0817e455c33e8cd5e88ff606a6757aa5a2
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2025-01-03 18:58:50 +0500

    class-46

---
+++ module 2/class-46/app.js
@@ -0,0 +1,34 @@
+class Point{
+
+    // this = {}
+
+    x ;
+    y; 
+    z;
+
+    constructor(x, y, z){
+        this.x = x;
+        this.y = y;
+        // this.z = z
+    }
+
+
+    // this = {
+    //     x: value;
+    //     y : value
+    // }
+}
+
+
+
+const pt = new Point(2, 6, 5);
+// const pt1 = new Point(3, 8);
+// const pt2 = new Point(7, 9);
+
+
+
+
+console.log(pt)
+// console.log(pt1)
+// console.log(pt2)
+
