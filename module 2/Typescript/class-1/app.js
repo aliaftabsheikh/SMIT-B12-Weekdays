5d087aa73328c225c8e28175bcde75b45a215afe
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2025-01-13 18:50:52 +0500

    Typescript-class-1

---
+++ module 2/Typescript/class-1/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstName = "Ali";
+let password = 121232332;
+let isAvailable = true;
+firstName = true;
+console.log(firstName);
---
+++ module 2/Typescript/class-1/app.ts
@@ -0,0 +1,9 @@
+let firstName: string = "Ali"
+
+let password: number = 121232332
+
+let isAvailable: boolean = true
+firstName = true
+
+
+console.log(firstName)
